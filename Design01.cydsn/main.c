f4806def275605309c77992ca30f7ac945c7b241
https://github.com/nichiwa-satellite/uart <okabe.tatsuya@nichiwa-system.co.jp>
2016-11-29 19:20:41 +0900

    登録するプロジェクトが違ったので、追加しなおし

--- Design01.cydsn/main.c
+++ Design01.cydsn/main.c
@@ -11,16 +11,18 @@
 */
 #include "project.h"
 
-int main(void)
+int main()
 {
     CyGlobalIntEnable; /* Enable global interrupts. */
-
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+    
 
+    UART_1_Start();
     for(;;)
     {
         /* Place your application code here. */
+        UART_1_PutString("test");
+        CyDelay(2000);
     }
 }
-
 /* [] END OF FILE */
